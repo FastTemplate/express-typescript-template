4a4b05167d582e94cbb60a765ef616429ee850e9
luohuidong <luohuidong01@126.com>
2020-10-12 23:23:59 +0800

    refactor: 修改 src 中的示例内容

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
-export default function sum(a: number, b: number): number {
-  return a + b;
-}
+import express from "express";
+
+import user from "./user";
+
+const app = express();
+const port = 8000;
+
+app.use("/user", user);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
